db48dae290d0630c50799cb662103f873ac647c0
icome2kill <nguyen.icome2kill.nguyen@gmail.com>
2013-09-22 15:38:01 +0700

    Update search function

--- Structured Programming/Controllers/SearchController.cs
+++ Structured Programming/Controllers/SearchController.cs
@@ -1,12 +1,12 @@
-﻿using Structured_Programming.Models;
+﻿using PagedList;
+using PagedList.Mvc;
+using Structured_Programming.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic;
 using System.Web;
 using System.Web.Mvc;
-using PagedList;
-using PagedList.Mvc;
 
 namespace Structured_Programming.Controllers
 {
@@ -16,10 +16,10 @@ namespace Structured_Programming.Controllers
         //
         // Simple search result: Search for item name only
         // GET: /Search/s={parameter}
-
         public ActionResult Index(int? page, SearchOptionsModel model)
         {
-            string s = model.Query;
+
+            string s = model.Query ?? "";
             // Flag to see if none of the search options ticked, default to search for Item name.
             bool flag = false;
             var pageNumber = page ?? 1;
@@ -27,7 +27,7 @@ namespace Structured_Programming.Controllers
             // List of items to display on result page
             List<Item> itemsToDisplay = new List<Item>();
 
-            // All items is queried first to minimize connection to database
+            // All items is queried first to minimize request to database
             var allItems = db.Items.ToList();
 
             if (model.InclName == true)
@@ -39,25 +39,34 @@ namespace Structured_Programming.Controllers
             {
                 flag = true;
                 itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Description.Contains(s))).ToList();
-            } 
+            }
             if (model.InclTrade == true)
             {
                 flag = true;
-                itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Trade.Contains(s))).ToList();
+                itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => (row.Trade != null) ? row.Trade.Contains(s) : false)).ToList();
             }
             if (model.InclUser == true)
             {
                 flag = true;
                 itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.UserProfile.UserName.Contains(s))).ToList();
             }
+            if (model.TypeId != 0)
+            {
+                itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.TypeId == model.TypeId)).ToList();
+            }
             if (flag == false)
             {
                 flag = true;
-                itemsToDisplay = itemsToDisplay = allItems.Where(row => row.Name.Contains(s)).ToList();
+                itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Name.Contains(s))).ToList();
+            }
+            if (model.TypeList == null)
+            {
+                model.TypeList = new SelectList(db.Types, "TypeId", "Name");
             }
             var vm = new SearchResultModel()
             {
-                Items = itemsToDisplay.ToPagedList(pageNumber, 3)
+                Items = itemsToDisplay.ToPagedList(pageNumber, 3),
+                CurrentOptions = model
             };
             return View(vm);
         }
--- Structured Programming/Models/SearchViewModels.cs
+++ Structured Programming/Models/SearchViewModels.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Structured_Programming.Models
 {
     public class SearchResultModel {
         public IEnumerable<Structured_Programming.Models.Item> Items {get; set;}
         public IEnumerable<Structured_Programming.Models.UserProfile> Users {get; set;}
+
+        public SearchOptionsModel CurrentOptions { get; set; }
     }
     public class SearchOptionsModel
     {
@@ -19,5 +22,8 @@ namespace Structured_Programming.Models
         public bool InclUser { get; set; }
         public bool InclDescription { get; set; }
         public bool InclTrade { get; set; }
+
+        public int TypeId { get; set; }
+        public SelectList TypeList { get; set; }
     }
 }
