644a35a5aff4396b1f59ed07a763eb952bcb2377
icome2kill <nguyen.icome2kill.nguyen@gmail.com>
2013-10-04 09:22:29 +0700

    Fix

---
+++ Structured Programming/Models/MetadataTypeModels/webpages_RolesMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Structured_Programming.Models
+{
+    [MetadataType(typeof(webpages_RolesMetadata))]
+    public partial class webpages_Roles
+    {
+    }
+    public class webpages_RolesMetadata
+    {
+        [Key]
+        public int RoleId { get; set; }
+    }
+}
