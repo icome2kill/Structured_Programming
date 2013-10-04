374fa6734258c957b700b1212aeae8eb2bef8852
icome2kill <nguyen.icome2kill.nguyen@gmail.com>
2013-10-04 11:59:32 +0700

    Added Transaction Index page.

---
+++ Structured Programming/Models/Business/AccountsManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Structured_Programming.Models.Business
+{
+    public class AccountsManagement
+    {
+        public static double Rating(UserProfile user)
+        {
+            double total = 0;
+            var rows = user.Rated.ToList();
+            foreach (var row in rows) {
+                total += row.Score;
+            }
+            return total / rows.Count;
+        }
+    }
+}
