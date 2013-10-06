351d149b1d779015a7ce80148f1649ba42304b2f
icome2kill <nguyen.icome2kill.nguyen@gmail.com>
2013-10-06 22:19:54 +0700

    Added error handling.

---
+++ Structured Programming/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Structured_Programming.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Error()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View("Error");
+        }
+
+    }
+}
--- Structured Programming/Models/Business/ItemsManagement.cs
+++ Structured Programming/Models/Business/ItemsManagement.cs
@@ -10,47 +10,52 @@ namespace Structured_Programming.Models.Business
         private static DataEntities db = new DataEntities();
         public static List<Item> ItemSearch(SearchOptionsModel model)
         {
-            string s = model.Query ?? "";
+            string str = model.Query ?? "";
             // Flag to see if none of the search options ticked, default to search for Item name.
             bool flag = false;
 
             // List of items to display on result page
             List<Item> itemsToDisplay = new List<Item>();
 
-            if (!s.Equals(""))
+            if (!str.Equals(""))
             {
                 // All items is queried first to minimize request to database
                 var allItems = db.Items.ToList();
 
-                if (model.InclName == true)
+                string[] splited = str.Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var s in splited)
                 {
-                    flag = true;
-                    itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Name.ToLower().Contains(s.ToLower()))).ToList();
-                }
-                if (model.InclDescription == true)
-                {
-                    flag = true;
-                    itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Description.ToLower().Contains(s.ToLower()))).ToList();
-                }
-                if (model.InclTrade == true)
-                {
-                    flag = true;
-                    itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => (row.Trade != null) ? row.Trade.ToLower().Contains(s.ToLower()) : false)).ToList();
-                }
-                if (model.InclUser == true)
-                {
-                    flag = true;
-                    itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.UserProfile.UserName.ToLower().Contains(s.ToLower()))).ToList();
-                }
-                if (model.TypeId != 0)
-                {
-                    itemsToDisplay = itemsToDisplay.Where(row => row.TypeId == model.TypeId).ToList();
-                }
-                if (flag == false)
-                {
-                    flag = true;
-                    model.InclName = true;
-                    itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Name.ToLower().Contains(s.ToLower()))).ToList();
+                    if (model.InclName == true)
+                    {
+                        flag = true;
+                        itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Name.ToLower().Contains(s.ToLower()))).ToList();
+                    }
+                    if (model.InclDescription == true)
+                    {
+                        flag = true;
+                        itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Description.ToLower().Contains(s.ToLower()))).ToList();
+                    }
+                    if (model.InclTrade == true)
+                    {
+                        flag = true;
+                        itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => (row.Trade != null) ? row.Trade.ToLower().Contains(s.ToLower()) : false)).ToList();
+                    }
+                    if (model.InclUser == true)
+                    {
+                        flag = true;
+                        itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.UserProfile.UserName.ToLower().Contains(s.ToLower()))).ToList();
+                    }
+                    if (model.TypeId != 0)
+                    {
+                        itemsToDisplay = itemsToDisplay.Where(row => row.TypeId == model.TypeId).ToList();
+                    }
+                    if (flag == false)
+                    {
+                        flag = true;
+                        model.InclName = true;
+                        itemsToDisplay = itemsToDisplay.Union(allItems.Where(row => row.Name.ToLower().Contains(s.ToLower()))).ToList();
+                    }
                 }
             }
             return itemsToDisplay;
